5df0cdd462554c52bf61b4bba163f2339888ea63
skfkgla <narahim.lee@gmail.com>
2022-06-22 14:20:56 +0900

    라우터 및 bootstrap 테스트

---
+++ src/Pages/registerPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+
+const RegisterPage = () => {
+  return (
+    <div>
+      Hello Register
+      <Button variant="warning">Warning</Button>
+    </div>
+  );
+};
+export default RegisterPage;
